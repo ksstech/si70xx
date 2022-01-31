d579ff6dd1da3dc98f29693b4c7d59cb8d3e678d
André Maree <ammaree@users.noreply.github.com>
2022-01-31 13:47:40 +0200

    Update si70xx.c

--- si70xx.c
+++ si70xx.c
@@ -24,9 +24,6 @@
 #define	debugPARAM					(debugFLAG_GLOBAL & debugFLAG & 0x4000)
 #define	debugRESULT					(debugFLAG_GLOBAL & debugFLAG & 0x8000)
 
-// ##################################### Developer notes ###########################################
-
-
 // ############################################# Macros ############################################
 
 #define	si70xxI2C_LOGIC				1					// 1=delay 2=stretch, 3=stages
